248b1ade7e7f06fc9d4a8a9c227e355377d2271a
nicollite <nickconte70@gmail.com>
2021-03-16 20:37:20 -0300

    change app component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { useLazyQuery } from "@apollo/client";
+import QueryBox, { QueryBoxHandleSearchOptions } from "./components/QueryBox";
+import Spinner from "./components/Spinner";
+import QuestionList from "./components/QuestionList";
+import { GET_QUESTIONS } from "./queries/question";
+import { Query } from "./graphql-types";
+import styled from "styled-components";
 
-function App() {
+const App = () => {
+  const [getQuestions, { loading, error, data, refetch }] = useLazyQuery<Query>(GET_QUESTIONS);
+  let [showLoading, setShowLoading] = useState(false);
+
+  async function handleSearch(variables: QueryBoxHandleSearchOptions) {
+    setShowLoading(true);
+    if (!data) getQuestions({ variables });
+    else if (refetch) await refetch(variables);
+
+    setShowLoading(false);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex column align-center">
+      <QueryBox handleSearch={handleSearch} />
+
+      {loading || showLoading ? (
+        <Spinner />
+      ) : error ? (
+        <ErrorContainer>
+          <span>Ocorreu um erro ao buscar os dados, por favor tente mais tarde</span>
+        </ErrorContainer>
+      ) : data ? (
+        <QuestionList questions={data.questions} />
+      ) : null}
     </div>
   );
-}
+};
 
 export default App;
+
+const ErrorContainer = styled.div`
+  padding: 15px 30px;
+  background-color: #ffd0cd;
+  color: #f00;
+`;
