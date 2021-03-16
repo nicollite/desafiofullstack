90ffc9ff5d2570920a06065365b827687c9cab3e
nicollite <nickconte70@gmail.com>
2021-03-16 20:07:56 -0300

    add Spinner component

---
+++ frontend/src/components/Spinner/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyledSpinner, StyledSpinnerWrapper } from "./styles";
+
+const Spinner = () => (
+  <StyledSpinnerWrapper>
+    <StyledSpinner viewBox="0 0 50 50">
+      <circle className="path" cx="25" cy="25" r="20" fill="none" strokeWidth="4" />
+    </StyledSpinner>
+  </StyledSpinnerWrapper>
+);
+
+export default Spinner;
